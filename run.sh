62923b27cfaff895038b3f81c547c7867c1b443d
Sungjae Lee <odobenuseKR@gmail.com>
2020-07-20 17:22:49 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,28 +8,29 @@ INSTANCE_TYPE=$6
 
 PROF_POINT=1.5
 
-# JOB_DIR="/home/ubuntu/Deep-Cloud/job/$MODEL_TYPE/$DATASET-$MODEL.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE"
+JOB_DIR="/home/ubuntu/Deep-Cloud/job/$MODEL_TYPE/$DATASET-$MODEL.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE"
 
-# # Get profile result at half of 2epoch
-# sudo -i -u root bash << EOF
+# Get profile result at half of 2epoch
+sudo -i -u root bash << EOF
 
-# python3.6 $JOB_DIR --batch_size 16 --prof_point PROF_POINT
-# sleep 3
+python3.6 $JOB_DIR --batch_size 16 --prof_point PROF_POINT
+sleep 3
 
-# python3.6 $JOB_DIR --batch_size 32 --prof_point PROF_POINT
-# sleep 3
+python3.6 $JOB_DIR --batch_size 32 --prof_point PROF_POINT
+sleep 3
 
-# python3.6 $JOB_DIR --batch_size 64 --prof_point PROF_POINT
-# sleep 3
+python3.6 $JOB_DIR --batch_size 64 --prof_point PROF_POINT
+sleep 3
 
-# python3.6 $JOB_DIR --batch_size 128 --prof_point PROF_POINT
-# sleep 3
+python3.6 $JOB_DIR --batch_size 128 --prof_point PROF_POINT
+sleep 3
 
-# python3.6 $JOB_DIR --batch_size 256 --prof_point PROF_POINT
-# sleep 3
+python3.6 $JOB_DIR --batch_size 256 --prof_point PROF_POINT
+sleep 3
 
-# EOF
+EOF
 
+# # Job Test
 # JOB_DIR1="/home/ubuntu/Deep-Cloud/job/cnn/mnist-lenet5.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
 # JOB_DIR2="/home/ubuntu/Deep-Cloud/job/cnn/mnist-resnetsmall.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
 # JOB_DIR3="/home/ubuntu/Deep-Cloud/job/cnn/mnist-vggsmall.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
@@ -40,37 +41,37 @@ PROF_POINT=1.5
 # JOB_DIR8="/home/ubuntu/Deep-Cloud/job/cnn/cifar10-resnetsmall.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
 # JOB_DIR9="/home/ubuntu/Deep-Cloud/job/cnn/cifar10-vggsmall.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
 
-JOB_DIR1="/home/ubuntu/Deep-Cloud/job/rnn/babi-rnn.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
-JOB_DIR2="/home/ubuntu/Deep-Cloud/job/rnn/babi-lstm.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
-JOB_DIR3="/home/ubuntu/Deep-Cloud/job/rnn/babi-bisltm.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
-JOB_DIR4="/home/ubuntu/Deep-Cloud/job/rnn/imdb-rnn.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
-JOB_DIR5="/home/ubuntu/Deep-Cloud/job/rnn/imdb-lstm.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
-JOB_DIR6="/home/ubuntu/Deep-Cloud/job/rnn/imdb-bisltm.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
-JOB_DIR7="/home/ubuntu/Deep-Cloud/job/rnn/reuters-rnn.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
-JOB_DIR8="/home/ubuntu/Deep-Cloud/job/rnn/reuters-lstm.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
-JOB_DIR9="/home/ubuntu/Deep-Cloud/job/rnn/reuters-bisltm.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
+# JOB_DIR1="/home/ubuntu/Deep-Cloud/job/rnn/babi-rnn.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
+# JOB_DIR2="/home/ubuntu/Deep-Cloud/job/rnn/babi-lstm.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
+# JOB_DIR3="/home/ubuntu/Deep-Cloud/job/rnn/babi-bisltm.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
+# JOB_DIR4="/home/ubuntu/Deep-Cloud/job/rnn/imdb-rnn.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
+# JOB_DIR5="/home/ubuntu/Deep-Cloud/job/rnn/imdb-lstm.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
+# JOB_DIR6="/home/ubuntu/Deep-Cloud/job/rnn/imdb-bisltm.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
+# JOB_DIR7="/home/ubuntu/Deep-Cloud/job/rnn/reuters-rnn.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
+# JOB_DIR8="/home/ubuntu/Deep-Cloud/job/rnn/reuters-lstm.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
+# JOB_DIR9="/home/ubuntu/Deep-Cloud/job/rnn/reuters-bisltm.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
 
 
-# Get profile result at half of 2epoch
-sudo -i -u root bash << EOF
+# # Get profile result at half of 2epoch
+# sudo -i -u root bash << EOF
 
-python3.6 $JOB_DIR1 --batch_size 64
-sleep 3
-python3.6 $JOB_DIR2 --batch_size 64
-sleep 3
-python3.6 $JOB_DIR3 --batch_size 64
-sleep 3
-python3.6 $JOB_DIR4 --batch_size 64
-sleep 3
-python3.6 $JOB_DIR5 --batch_size 64
-sleep 3
-python3.6 $JOB_DIR6 --batch_size 64
-sleep 3
-python3.6 $JOB_DIR7 --batch_size 64
-sleep 3
-python3.6 $JOB_DIR8 --batch_size 64
-sleep 3
-python3.6 $JOB_DIR9 --batch_size 64
-sleep 3
+# python3.6 $JOB_DIR1 --batch_size 64
+# sleep 3
+# python3.6 $JOB_DIR2 --batch_size 64
+# sleep 3
+# python3.6 $JOB_DIR3 --batch_size 64
+# sleep 3
+# python3.6 $JOB_DIR4 --batch_size 64
+# sleep 3
+# python3.6 $JOB_DIR5 --batch_size 64
+# sleep 3
+# python3.6 $JOB_DIR6 --batch_size 64
+# sleep 3
+# python3.6 $JOB_DIR7 --batch_size 64
+# sleep 3
+# python3.6 $JOB_DIR8 --batch_size 64
+# sleep 3
+# python3.6 $JOB_DIR9 --batch_size 64
+# sleep 3
 
-EOF
+# EOF
