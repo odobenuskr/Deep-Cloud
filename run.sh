cb8e97ef6a50351432a6559d09217f1c53bceec3
Sungjae Lee <odobenuseKR@gmail.com>
2020-07-20 17:31:37 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,24 +8,24 @@ INSTANCE_TYPE=$6
 
 PROF_POINT=1.5
 
-JOB_DIR="/home/ubuntu/Deep-Cloud/job/$MODEL_TYPE/$DATASET-$MODEL.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE"
+JOB_DIR="/home/ubuntu/Deep-Cloud/job/$MODEL_TYPE/$DATASET-$MODEL.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
 
 # Get profile result at half of 2epoch
 sudo -i -u root bash << EOF
 
-python3.6 $JOB_DIR --batch_size 16 --prof_point PROF_POINT
+python3.6 $JOB_DIR --batch_size 16 
 sleep 3
 
-python3.6 $JOB_DIR --batch_size 32 --prof_point PROF_POINT
+python3.6 $JOB_DIR --batch_size 32
 sleep 3
 
-python3.6 $JOB_DIR --batch_size 64 --prof_point PROF_POINT
+python3.6 $JOB_DIR --batch_size 64
 sleep 3
 
-python3.6 $JOB_DIR --batch_size 128 --prof_point PROF_POINT
+python3.6 $JOB_DIR --batch_size 128
 sleep 3
 
-python3.6 $JOB_DIR --batch_size 256 --prof_point PROF_POINT
+python3.6 $JOB_DIR --batch_size 256
 sleep 3
 
 EOF
