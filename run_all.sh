bd495659ec133de41cd339c091c3424a8bb58e2c
Sungjae Lee <odobenuseKR@gmail.com>
2020-07-21 15:33:33 +0900

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -8,7 +8,7 @@ INSTANCE_TYPE=$6
 
 PROF_POINT=1.5
 
-# Job Test
+# CNN Job Test
 JOB_DIR1="/home/ubuntu/Deep-Cloud/job/cnn/mnist-lenet5.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
 JOB_DIR2="/home/ubuntu/Deep-Cloud/job/cnn/mnist-resnetsmall.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
 JOB_DIR3="/home/ubuntu/Deep-Cloud/job/cnn/mnist-vggsmall.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
@@ -19,6 +19,111 @@ JOB_DIR7="/home/ubuntu/Deep-Cloud/job/cnn/cifar10-lenet5.py --optimizer $OPTIMIZ
 JOB_DIR8="/home/ubuntu/Deep-Cloud/job/cnn/cifar10-resnetsmall.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
 JOB_DIR9="/home/ubuntu/Deep-Cloud/job/cnn/cifar10-vggsmall.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
 
+# Get profile result
+sudo -i -u root bash << EOF
+
+python3.6 $JOB_DIR1 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR1 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR1 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR1 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR1 --batch_size 256
+sleep 3
+
+python3.6 $JOB_DIR2 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR2 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR2 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR2 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR2 --batch_size 256
+sleep 3
+
+python3.6 $JOB_DIR3 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR3 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR3 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR3 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR3 --batch_size 256
+sleep 3
+
+python3.6 $JOB_DIR4 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR4 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR4 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR4 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR4 --batch_size 256
+sleep 3
+
+python3.6 $JOB_DIR5 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR5 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR5 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR5 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR5 --batch_size 256
+sleep 3
+
+python3.6 $JOB_DIR6 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR6 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR6 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR6 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR6 --batch_size 256
+sleep 3
+
+python3.6 $JOB_DIR7 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR7 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR7 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR7 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR7 --batch_size 256
+sleep 3
+
+python3.6 $JOB_DIR8 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR8 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR8 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR8 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR8 --batch_size 256
+sleep 3
+
+python3.6 $JOB_DIR9 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR9 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR9 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR9 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR9 --batch_size 256
+sleep 3
+
+EOF
+
+# RNN Job Test
 JOB_DIR1="/home/ubuntu/Deep-Cloud/job/rnn/babi-rnn.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
 JOB_DIR2="/home/ubuntu/Deep-Cloud/job/rnn/babi-lstm.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
 JOB_DIR3="/home/ubuntu/Deep-Cloud/job/rnn/babi-bisltm.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
@@ -29,26 +134,106 @@ JOB_DIR7="/home/ubuntu/Deep-Cloud/job/rnn/reuters-rnn.py --optimizer $OPTIMIZER 
 JOB_DIR8="/home/ubuntu/Deep-Cloud/job/rnn/reuters-lstm.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
 JOB_DIR9="/home/ubuntu/Deep-Cloud/job/rnn/reuters-bisltm.py --optimizer $OPTIMIZER --prof_or_latency $PROF_MODE --prof_point $PROF_POINT"
 
-# # Get profile result at half of 2epoch
-# sudo -i -u root bash << EOF
+# Get profile result
+sudo -i -u root bash << EOF
 
-# python3.6 $JOB_DIR1 --batch_size 64
-# sleep 3
-# python3.6 $JOB_DIR2 --batch_size 64
-# sleep 3
-# python3.6 $JOB_DIR3 --batch_size 64
-# sleep 3
-# python3.6 $JOB_DIR4 --batch_size 64
-# sleep 3
-# python3.6 $JOB_DIR5 --batch_size 64
-# sleep 3
-# python3.6 $JOB_DIR6 --batch_size 64
-# sleep 3
-# python3.6 $JOB_DIR7 --batch_size 64
-# sleep 3
-# python3.6 $JOB_DIR8 --batch_size 64
-# sleep 3
-# python3.6 $JOB_DIR9 --batch_size 64
-# sleep 3
+python3.6 $JOB_DIR1 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR1 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR1 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR1 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR1 --batch_size 256
+sleep 3
 
-# EOF
+python3.6 $JOB_DIR2 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR2 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR2 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR2 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR2 --batch_size 256
+sleep 3
+
+python3.6 $JOB_DIR3 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR3 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR3 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR3 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR3 --batch_size 256
+sleep 3
+
+python3.6 $JOB_DIR4 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR4 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR4 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR4 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR4 --batch_size 256
+sleep 3
+
+python3.6 $JOB_DIR5 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR5 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR5 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR5 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR5 --batch_size 256
+sleep 3
+
+python3.6 $JOB_DIR6 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR6 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR6 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR6 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR6 --batch_size 256
+sleep 3
+
+python3.6 $JOB_DIR7 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR7 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR7 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR7 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR7 --batch_size 256
+sleep 3
+
+python3.6 $JOB_DIR8 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR8 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR8 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR8 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR8 --batch_size 256
+sleep 3
+
+python3.6 $JOB_DIR9 --batch_size 16
+sleep 3
+python3.6 $JOB_DIR9 --batch_size 32
+sleep 3
+python3.6 $JOB_DIR9 --batch_size 64
+sleep 3
+python3.6 $JOB_DIR9 --batch_size 128
+sleep 3
+python3.6 $JOB_DIR9 --batch_size 256
+sleep 3
+
+EOF
