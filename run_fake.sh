daa2ffcb9948231ed2141a740281918a4f1adf05
Sungjae Lee <odobenuseKR@gmail.com>
2020-10-12 17:03:04 +0900

    Update run_fake.sh

--- run_fake.sh
+++ run_fake.sh
@@ -1,11 +1,11 @@
 # CNN Job Test
-JOB_DIR1="/home/ubuntu/Deep-Cloud/job/cnn/fake/fake-mnist-lenet5.py"
-JOB_DIR2="/home/ubuntu/Deep-Cloud/job/cnn/fake/fake-fmnist-resnetsmall.py"
-JOB_DIR3="/home/ubuntu/Deep-Cloud/job/cnn/fake/fake-cifar10-vggsmall.py"
+JOB_DIR1="/home/ubuntu/Deep-Cloud/job/fake/fake-mnist-lenet5.py"
+JOB_DIR2="/home/ubuntu/Deep-Cloud/job/fake/fake-fmnist-resnetsmall.py"
+JOB_DIR3="/home/ubuntu/Deep-Cloud/job/fake/fake-cifar10-vggsmall.py"
 
-JOB_DIR4="/home/ubuntu/Deep-Cloud/job/cnn/real/real-mnist-lenet5.py"
-JOB_DIR5="/home/ubuntu/Deep-Cloud/job/cnn/real/real-fmnist-resnetsmall.py"
-JOB_DIR6="/home/ubuntu/Deep-Cloud/job/cnn/real/real-cifar10-vggsmall.py"
+JOB_DIR4="/home/ubuntu/Deep-Cloud/job/real/real-mnist-lenet5.py"
+JOB_DIR5="/home/ubuntu/Deep-Cloud/job/real/real-fmnist-resnetsmall.py"
+JOB_DIR6="/home/ubuntu/Deep-Cloud/job/real/real-cifar10-vggsmall.py"
 
 # Get profile result
 sudo -i -u root bash << EOF
