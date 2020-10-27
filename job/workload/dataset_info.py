fe6b6cb6786798612998dd9930de9ca7f0e51086
Sungjae Lee <odobenuseKR@gmail.com>
2020-10-27 10:06:48 +0900

    Add files via upload

---
+++ job/workload/dataset_info.py
@@ -0,0 +1,53 @@
+dataset_16 = {
+    'num_classes': 10,
+    'img_rows': 16,
+    'img_cols': 16,
+    'img_channels': 3,
+    'num_data': 50000,
+    'num_test': 10000
+}
+
+dataset_32 = {
+    'num_classes': 10,
+    'img_rows': 32,
+    'img_cols': 32,
+    'img_channels': 3,
+    'num_data': 50000,
+    'num_test': 10000
+}
+
+dataset_64 = {
+    'num_classes': 100,
+    'img_rows': 64,
+    'img_cols': 64,
+    'img_channels': 3,
+    'num_data': 5000,
+    'num_test': 1000
+} 
+
+dataset_128 = {
+    'num_classes': 100,
+    'img_rows': 128,
+    'img_cols': 128,
+    'img_channels': 3,
+    'num_data': 5000,
+    'num_test': 1000
+}
+
+dataset_224 = {
+    'num_classes': 1000,
+    'img_rows': 224,
+    'img_cols': 224,
+    'img_channels': 3,
+    'num_data': 500,
+    'num_test': 100
+}
+
+dataset_256 = {
+    'num_classes': 1000,
+    'img_rows': 256,
+    'img_cols': 256,
+    'img_channels': 3,
+    'num_data': 500,
+    'num_test': 100
+}
