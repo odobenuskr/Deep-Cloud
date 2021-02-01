168b8108f489dbaf57380a2d39fb2654fbde122a
Sungjae Lee <odobenuseKR@gmail.com>
2021-02-02 00:20:29 +0900

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -6,7 +6,7 @@ ln -s /usr/local/cuda-10.1 /usr/local/cuda
 
 # Upgrade pip3 and install tensorflow, tensorboard, dataset packages
 pip3 install --upgrade pip
-pip3 install tf-nightly
+pip3 install tensorflow==2.2
 #pip3 uninstall tensorboard
 #pip3 install tensorboard==2.2
 pip3 install tensorboard_plugin_profile
